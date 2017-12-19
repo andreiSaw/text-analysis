e532861cab01b3cf23dfd5c4b1eb9a632e395c54
macbook <isaevnextdoor@gmail.com>
2017-12-19 04:21:36 +0300

    age+edu

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from sklearn.externals import joblib
 import scipy.sparse as sp
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 
 class Solution:  # Class must have name "Solution"
@@ -16,11 +16,13 @@ class Solution:  # Class must have name "Solution"
 
     def train(self, training_corpus):
         train_df = pd.DataFrame.from_dict(training_corpus)
+
         Z = train_df
 
         Z.loc[:, 'q'] = pd.Series(np.random.randn(len(Z)), index=Z.index)
+
         for index, row in Z.iterrows():
-            Z.at[index, 'text'] = ''.join(Z.at[index, 'texts']).lower() \
+            Z.at[index, 'text'] = ' '.join(Z.at[index, 'texts']).lower() \
                 .replace('.', ' ') \
                 .replace(',', ' ') \
                 .replace(';', ' ') \
@@ -34,7 +36,7 @@ class Solution:  # Class must have name "Solution"
                         "education": {"lower": 1, "middle": 2, "high": 3}}
         Z.replace(cleanup_nums, inplace=True)
 
-        Z.rename(columns={'education': 'y'}, inplace=True)
+        Z.rename(columns={'age': 'y'}, inplace=True)
 
         train_sels = ~np.isnan(Z.y)
         test_sels = np.isnan(Z.y)
@@ -62,7 +64,7 @@ class Solution:  # Class must have name "Solution"
 
         print("training LR\n")
 
-        clf = LogisticRegression(C=1.0)
+        clf = SVC(C=1.0)
         clf.fit(train_f, y_train)
 
         print("dumped LR\n")
@@ -70,16 +72,26 @@ class Solution:  # Class must have name "Solution"
         joblib.dump(clf, 'm.pkl')
 
     def get_age(self, texts):
-        """Returns age for author of the input texts
-             :param texts: list of texts for processing
-             :return: age interval
-             """
-        return "25-34"
+        model1 = joblib.load('m.pkl')
+
+        df = pd.DataFrame({"text": " ".join(texts)}, index=[0])
+
+        tit_test_features_cv = self.tfidf.transform(df['text'].values)
+        text_test_f = self.vectorizer.transform(df["text"].values)
+        test_f = sp.hstack((text_test_f, tit_test_features_cv), format='csr')
+
+        df_t = model1.predict(test_f)
+        df__3 = pd.DataFrame({"age": df_t}, index=[0])
+
+        cleanup_nums = {"age": {1.0: "<=17", 2.0: "18-24", 3.0: "25-34", 4.0: "35-44", 5.0: ">=45"}}
+        df__3.replace(cleanup_nums, inplace=True)
+
+        return df__3.at[0, "age"]
 
     def get_education(self, texts):
         model1 = joblib.load('m.pkl')
 
-        df = pd.DataFrame({"text": "".join(texts).lower()
+        df = pd.DataFrame({"text": " ".join(texts).lower()
                           .replace('.', ' ') \
                           .replace(',', ' ') \
                           .replace(';', ' ') \
